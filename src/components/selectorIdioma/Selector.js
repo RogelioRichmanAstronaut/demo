9791e485c28b89b075d5830c7b9db4731f402c71
RogelioRichmanAstronaut <56854119+RogelioRichmanAstronaut@users.noreply.github.com>
2022-11-16 21:29:41 -0500

    link a poster online
    xd

--- src/components/selectorIdioma/Selector.js
+++ src/components/selectorIdioma/Selector.js
@@ -11,10 +11,12 @@ const Selector = () => {
   return (
     <div className='principali'>
         <div className='contenedor'>
+        <a href="https://www.canva.com/design/DAFR-TmuaoA/LXW4x1Y1RgK9Of6KU4NhOg/view?utm_content=DAFR-TmuaoA&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink" target="_blank">Póster online {">"}</a>
             <h1>
                 DEMO VAUPÉS
             </h1>
             <h3> SELECCIONA UN IDIOMA</h3>
+            
             <div className='navigation'>
                 <button onClick={() => clickLanguage('en')}>Español</button>
                 <button onClick={() => clickLanguage('si')}>Siriano</button>
